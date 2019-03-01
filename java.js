1ab3de38ddeaea48a75720302d1eb18645824f9d
osvaldoarreola <37843538+osvaldoarreola@users.noreply.github.com>
2019-03-01 15:30:56 -0800

    Add files via upload

--- java.js
+++ java.js
@@ -1,1 +1,110 @@
 
+function nextImage(el){
+	if (el.src.match("images/token.png")){
+		el.src = "image/kenny.png";
+	} else if (el.src.match("image/kenny.png")){
+		el.src = "image/stan.png";
+	} else if (el.src.match("image/stan.png")){
+		el.src = "image/tweak.png";
+	} else {
+	}
+}
+
+function nextImage(el){
+	if (el.src.match("images/token.png")){
+		el.src = "image/kenny.png";
+	} else if (el.src.match("image/kenny.png")){
+		el.src = "image/stan.png";
+	} else if (el.src.match("image/stan.png")){
+		el.src = "image/tweak.png";
+	} else {
+	}
+}
+
+function nextImage(el){
+	if (el.src.match("images/token.png")){
+		el.src = "image/kenny.png";
+	} else if (el.src.match("image/kenny.png")){
+		el.src = "image/stan.png";
+	} else if (el.src.match("image/stan.png")){
+		el.src = "image/tweak.png";
+	} else {
+	}
+}
+
+function nextImage(el){
+	if (el.src.match("images/token.png")){
+		el.src = "image/kenny.png";
+	} else if (el.src.match("image/kenny.png")){
+		el.src = "image/stan.png";
+	} else if (el.src.match("image/stan.png")){
+		el.src = "image/tweak.png";
+	} else {
+	}
+}
+
+function nextImage(el){
+	if (el.src.match("images/token.png")){
+		el.src = "image/kenny.png";
+	} else if (el.src.match("image/kenny.png")){
+		el.src = "image/stan.png";
+	} else if (el.src.match("image/stan.png")){
+		el.src = "image/tweak.png";
+	} else {
+	}
+}
+
+function nextImage(el){
+	if (el.src.match("images/token.png")){
+		el.src = "image/kenny.png";
+	} else if (el.src.match("image/kenny.png")){
+		el.src = "image/stan.png";
+	} else if (el.src.match("image/stan.png")){
+		el.src = "image/tweak.png";
+	} else {
+	}
+}
+
+function nextImage(el){
+	if (el.src.match("images/token.png")){
+		el.src = "image/kenny.png";
+	} else if (el.src.match("image/kenny.png")){
+		el.src = "image/stan.png";
+	} else if (el.src.match("image/stan.png")){
+		el.src = "image/tweak.png";
+	} else {
+	}
+}
+
+function nextImage(el){
+	if (el.src.match("images/token.png")){
+		el.src = "image/kenny.png";
+	} else if (el.src.match("image/kenny.png")){
+		el.src = "image/stan.png";
+	} else if (el.src.match("image/stan.png")){
+		el.src = "image/tweak.png";
+	} else {
+	}
+}
+
+function nextImage(el){
+	if (el.src.match("images/token.png")){
+		el.src = "image/kenny.png";
+	} else if (el.src.match("image/kenny.png")){
+		el.src = "image/stan.png";
+	} else if (el.src.match("image/stan.png")){
+		el.src = "image/tweak.png";
+	} else {
+	}
+}
+
+function nextImage(el){
+	if (el.src.match("images/token.png")){
+		el.src = "image/kenny.png";
+	} else if (el.src.match("image/kenny.png")){
+		el.src = "image/stan.png";
+	} else if (el.src.match("image/stan.png")){
+		el.src = "image/tweak.png";
+	} else {
+	}
+}
